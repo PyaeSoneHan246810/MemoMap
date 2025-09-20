96408112524a904682e855209e457f1ab7f27a7f
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-09-20 22:23:27 +0700

    Create swiftui project

---
+++ MemoMap/MemoMap/MemoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoMapApp.swift
+//  MemoMap
+//
+//  Created by Dylan on 20/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
