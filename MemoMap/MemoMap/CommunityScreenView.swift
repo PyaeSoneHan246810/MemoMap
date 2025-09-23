5f5f5b209ecd85806ebd368e9d7f044c51f1a852
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-09-24 01:19:41 +0700

    [Feature] Develop ui for community screen including user profile view component and modify the tab bar not to be visible on settings screen

---
+++ MemoMap/MemoMap/CommunityScreenView.swift
@@ -0,0 +1,76 @@
+//
+//  CommunityScreenView.swift
+//  MemoMap
+//
+//  Created by Dylan on 24/9/25.
+//
+
+import SwiftUI
+
+struct CommunityScreenView: View {
+    @State private var selectedConnectionType: ConnectionType = .followers
+    var body: some View {
+        VStack(spacing: 0.0) {
+            connectionTypePickerView
+            connectionsView
+        }
+        .navigationTitle("Community")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            toolbarContentView
+        }
+        .toolbarVisibility(.hidden, for: .tabBar)
+    }
+}
+
+extension CommunityScreenView {
+    enum ConnectionType: Hashable {
+        case followers
+        case following
+        case mutuals
+    }
+}
+
+private extension CommunityScreenView {
+    @ToolbarContentBuilder
+    var toolbarContentView: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            NavigationLink {
+                
+            } label: {
+                Label("Follow user", systemImage: "person.badge.plus")
+            }
+        }
+    }
+    var connectionTypePickerView: some View {
+        Picker("Connection Type", selection: $selectedConnectionType) {
+            Text("10 Followers")
+                .tag(ConnectionType.followers)
+            Text("10 Followings")
+                .tag(ConnectionType.following)
+            Text("4 Mutuals")
+                .tag(ConnectionType.mutuals)
+        }
+        .pickerStyle(.segmented)
+        .padding(16.0)
+    }
+    var connectionsView: some View {
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 16.0) {
+                ForEach(0...5, id: \.self) { _ in
+                    UserProfileView(
+                        userProfileInfo: UserProfileView.previewUserProfileInfo1
+                    )
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+        .contentMargins(16.0)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        CommunityScreenView()
+    }
+}
---
+++ MemoMap/MemoMap/UserProfileView.swift
@@ -0,0 +1,103 @@
+//
+//  UserProfileView.swift
+//  MemoMap
+//
+//  Created by Dylan on 24/9/25.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    let userProfileInfo: UserProfileInfo
+    var body: some View {
+        HStack(spacing: 12.0) {
+            profilePhotoView
+            userInfoView
+            actionButtonView
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+extension UserProfileView {
+    enum UserType {
+        case following
+        case unfollowed
+        case mutual
+    }
+    struct UserProfileInfo {
+        let profilePhotoUrl: String
+        let displayName: String
+        let username: String
+        let userType: UserType
+    }
+    static let previewUserProfileInfo1: UserProfileInfo = .init(
+        profilePhotoUrl: "",
+        displayName: "",
+        username: "",
+        userType: .following
+    )
+    static let previewUserProfileInfo2: UserProfileInfo = .init(
+        profilePhotoUrl: "",
+        displayName: "",
+        username: "",
+        userType: .unfollowed
+    )
+}
+
+private extension UserProfileView {
+    var profilePhotoView: some View {
+        Image(.profilePlaceholder)
+            .resizable()
+            .frame(width: 60.0, height: 60.0)
+            .foregroundStyle(Color(uiColor: .secondarySystemBackground))
+    }
+    var userInfoView: some View {
+        VStack(alignment: .leading, spacing: 2.0) {
+            Text("Display Name")
+                .font(.callout)
+                .fontWeight(.medium)
+            Text("@username")
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    @ViewBuilder
+    var actionButtonView: some View {
+        switch userProfileInfo.userType {
+        case .following:
+            followingButtonView
+        case .unfollowed:
+            followButtonView
+        case .mutual:
+            followingButtonView
+        }
+    }
+    var followingButtonView: some View {
+        Button("Following") {
+            
+        }
+        .buttonStyle(.bordered)
+        .foregroundStyle(.primary)
+        .controlSize(.small)
+        .font(.footnote)
+    }
+    var followButtonView: some View {
+        Button("Follow", systemImage: "person.badge.plus") {
+            
+        }
+        .buttonStyle(.borderedProminent)
+        .controlSize(.small)
+        .font(.footnote)
+    }
+}
+
+#Preview {
+    UserProfileView(
+        userProfileInfo: UserProfileView.previewUserProfileInfo1
+    )
+    UserProfileView(
+        userProfileInfo: UserProfileView.previewUserProfileInfo2
+    )
+}
