6a6fd8d4bf5e78a90eb8937c28e836c6da03201c
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-09-29 15:07:19 +0700

    [Feature] Develop locate me button that displays on map

--- MemoMap/MemoMap/Presentation/MemoriesMap/MemoriesScreenView.swift
+++ MemoMap/MemoMap/Presentation/MemoriesMap/MemoriesScreenView.swift
@@ -10,7 +10,7 @@ import MapboxMaps
 
 struct MemoriesScreenView: View {
     @Environment(\.colorScheme) private var colorScheme
-    @State private var viewport: Viewport = .followPuck(zoom: 12.0)
+    @State private var viewport: Viewport = .followPuck(zoom: 12.0, bearing: .constant(0.0), pitch: 0.0)
     @State private var placeTapped: Place? = nil
     var body: some View {
         mapView
@@ -64,6 +64,10 @@ private extension MemoriesScreenView {
             }
         }
         .mapStyle(mapStyle)
+        .overlay(alignment: .trailing) {
+            LocateMeButton(viewport: $viewport)
+                .padding(8.0)
+        }
     }
     @ToolbarContentBuilder
     var toolbarContentView: some ToolbarContent {
@@ -85,6 +89,42 @@ private extension MemoriesScreenView {
         NavigationStack {
             AddNewPinView()
         }
+    }
+}
+
+struct LocateMeButton: View {
+    @Binding var viewport: Viewport
+    var body: some View {
+        Button {
+            withViewportAnimation(.default(maxDuration: 1)) {
+                if isFocusingUser {
+                    viewport = .followPuck(zoom: 16.0, bearing: .heading, pitch: 60.0)
+                } else {
+                    viewport = .followPuck(zoom: 12.0, bearing: .constant(0), pitch: 0.0)
+                }
+            }
+        } label: {
+            Image(systemName: imageName)
+                .imageScale(.large)
+                .contentTransition(.symbolEffect(.replace))
+        }
+        .buttonStyle(.glass)
+        .buttonBorderShape(.circle)
+        .controlSize(.large)
+    }
+    private var isFocusingUser: Bool {
+        return viewport.followPuck?.bearing == .constant(0)
+    }
+    private var isFollowingUser: Bool {
+        return viewport.followPuck?.bearing == .heading
+    }
+    private var imageName: String {
+        if isFocusingUser {
+           return  "location.fill"
+        } else if isFollowingUser {
+           return "location.north.line.fill"
+        }
+        return "location"
     }
 }
 
