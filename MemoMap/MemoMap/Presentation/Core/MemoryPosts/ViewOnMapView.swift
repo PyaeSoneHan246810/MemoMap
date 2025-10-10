37a694a63996f31cd10c6b798e97072ea10dcc68
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-10-11 02:53:01 +0700

    [Feature] Develop functionality to view the location of the memory on map

--- MemoMap/MemoMap/Presentation/Core/MemoryPosts/ViewOnMapView.swift
+++ MemoMap/MemoMap/Presentation/Core/MemoryPosts/ViewOnMapView.swift
@@ -11,15 +11,22 @@ import MapboxMaps
 struct ViewOnMapView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.colorScheme) private var colorScheme: ColorScheme
+    let locationName: String
+    let latitude: Double
+    let longitude: Double
+    private var pinCoordinate: CLLocationCoordinate2D {
+        .init(latitude: latitude, longitude: longitude)
+    }
+    private var initialViewport: Viewport {
+        .camera(center: pinCoordinate, zoom: 16.0, pitch: 20.0)
+    }
     private var toolbarbackground: Color {
         colorScheme == .dark ? Color.black : Color.white
     }
     var body: some View {
-        Map {
-            
-        }
+        mapView
         .ignoresSafeArea()
-        .navigationTitle("Location name")
+        .navigationTitle(locationName)
         .navigationBarTitleDisplayMode(.inline)
         .toolbarBackgroundVisibility(.visible, for: .navigationBar)
         .toolbarBackground(toolbarbackground, for: .navigationBar)
@@ -42,10 +49,41 @@ private extension ViewOnMapView {
             }
         }
     }
+    var mapStyle: MapStyle {
+        switch colorScheme {
+        case .dark:
+            MapStyle.standard(lightPreset: .night)
+        case .light:
+            MapStyle.standard(lightPreset: .day)
+        @unknown default:
+            MapStyle.standard
+        }
+    }
+    var mapView: some View {
+        Map(initialViewport: initialViewport) {
+            pinPointAnnotation
+            userLocationPuck
+        }
+        .mapStyle(mapStyle)
+    }
+    var pinPointAnnotation: some MapContent {
+        PointAnnotation(
+            point: Point(pinCoordinate)
+        )
+        .image(.init(image: UIImage(resource: .pin), name: "pin"))
+    }
+    var userLocationPuck: some MapContent {
+        Puck2D(bearing: .heading)
+            .showsAccuracyRing(true)
+    }
 }
 
 #Preview {
     NavigationStack {
-        ViewOnMapView()
+        ViewOnMapView(
+            locationName: "Central World",
+            latitude: 13.7465,
+            longitude: 100.5391,
+        )
     }
 }
